be11bc7d8d9ac652551e7724e105ec14e7f6280c
lehuynhphat2808 <lehuynhphat2808@gmail.com>
2025-04-16 23:49:41 +0700

    feat: theme and main

---
+++ lib/ui/theme/app.theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class AppTheme {
+  // Light Theme
+  static ThemeData lightMode = ThemeData(
+    appBarTheme: AppBarTheme(backgroundColor: Color(0xff5372F0))
+      );
+
+}
